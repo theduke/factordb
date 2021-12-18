636e877d23a98a07271f3c23b933a8988ba88b0e
Christoph Herzog <chris@theduke.at>
2021-12-18 23:55:56 +0100

    perf: Use InLiteral for entity type inherits query
    Improves performance for a common query mode.

--- factordb/src/backend/query_planner.rs
+++ factordb/src/backend/query_planner.rs
@@ -144,17 +144,17 @@ pub fn resolve_expr(expr: Expr, reg: &Registry) -> Result<ResolvedExpr, AnyError
             // Must be a cleaner way to structure this!
             // Probably want a dedicted expr to check the type!
             let ty = reg.require_entity_by_name(&type_name)?;
-            let mut children: Vec<_> = ty
+            let mut items: HashSet<_> = ty
                 .nested_children
                 .iter()
                 .filter_map(|id| Some(Value::from(reg.entity_by_id(*id)?.schema.ident.clone())))
                 .collect();
-            children.push(ty.schema.ident.clone().into());
-            Ok(ResolvedExpr::BinaryOp(Box::new(BinaryExpr {
-                left: ResolvedExpr::Attr(ATTR_TYPE_LOCAL),
-                op: BinaryOp::In,
-                right: ResolvedExpr::Literal(Value::List(children)),
-            })))
+            items.insert(ty.schema.ident.clone().into());
+
+            Ok(ResolvedExpr::InLiteral {
+                value: Box::new(ResolvedExpr::Attr(ATTR_TYPE_LOCAL)),
+                items,
+            })
         }
     }
 }
