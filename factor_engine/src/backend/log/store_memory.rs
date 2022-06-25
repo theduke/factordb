8d6357bdec1be96aae68bb667c0123f6fa6be7f7
Christoph Herzog <chris@theduke.at>
2022-06-25 04:06:47 +0200

    Make MemoryLogStore shareable
    Put data behind an Arc<RwLock<_>>

--- factor_engine/src/backend/log/store_memory.rs
+++ factor_engine/src/backend/log/store_memory.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, RwLock};
+
 use futures::{
     future::{ready, BoxFuture},
     stream::BoxStream,
@@ -10,8 +12,9 @@ use super::{EventId, LogEvent};
 
 /// Mock memory log store.
 /// Only useful for testing.
+#[derive(Clone)]
 pub struct MemoryLogStore {
-    events: std::collections::BTreeMap<super::EventId, LogEvent>,
+    events: Arc<RwLock<std::collections::BTreeMap<super::EventId, LogEvent>>>,
 }
 
 impl MemoryLogStore {
@@ -34,20 +37,32 @@ impl super::LogStore for MemoryLogStore {
     fn iter_events(&self, from: super::EventId, until: super::EventId) -> StreamFuture {
         let stream = self
             .events
+            .read()
+            .unwrap()
             .range(from..until)
-            .map(|(_key, value)| Ok(value.clone()));
+            .map(|(_key, value)| Ok(value.clone()))
+            .collect::<Vec<_>>();
         let boxed_stream = futures::stream::iter(stream).boxed();
         let res = Ok(boxed_stream);
         ready(res).boxed()
     }
 
     fn read_event(&self, id: EventId) -> BoxFuture<Result<Option<LogEvent>, AnyError>> {
-        let res = self.events.get(&id).cloned().map(Ok).transpose();
+        let res = self
+            .events
+            .read()
+            .unwrap()
+            .get(&id)
+            .cloned()
+            .map(Ok)
+            .transpose();
         ready(res).boxed()
     }
 
     fn write_event(&mut self, event: LogEvent) -> BoxFuture<Result<(), AnyError>> {
-        let expected_id = self.events.len() as u64 + 1;
+        let mut events = self.events.write().unwrap();
+
+        let expected_id = events.len() as u64 + 1;
 
         let res = if event.id != expected_id {
             Err(anyhow::anyhow!(
@@ -56,7 +71,7 @@ impl super::LogStore for MemoryLogStore {
                 event.id,
             ))
         } else {
-            self.events.insert(event.id, event);
+            events.insert(event.id, event);
             Ok(())
         };
 
@@ -64,7 +79,7 @@ impl super::LogStore for MemoryLogStore {
     }
 
     fn clear(&mut self) -> BoxFuture<'static, Result<(), AnyError>> {
-        self.events.clear();
+        self.events.write().unwrap().clear();
         ready(Ok(())).boxed()
     }
 
