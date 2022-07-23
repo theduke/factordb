72399793a3a8448268449264fe26b8eee10bf9d2
Christoph Herzog <chris@theduke.at>
2022-07-23 21:58:44 +0200

    Add unify_migrations helper
    Can be useful for merging previous migrations for a new database.

--- factordb/src/query/migrate.rs
+++ factordb/src/query/migrate.rs
@@ -1,3 +1,6 @@
+use anyhow::{anyhow, bail};
+use schema::{AttributeSchema, EntitySchema};
+
 use crate::{
     data::Value,
     prelude::ValueType,
@@ -258,4 +261,166 @@ impl Migration {
         }));
         self
     }
+}
+
+/// Convert a list of migrations into a single migration that re-creates
+/// the final state.
+pub fn unify_migrations(migrations: Vec<Migration>) -> Result<Migration, anyhow::Error> {
+    let mut attributes = Vec::<AttributeSchema>::new();
+    let mut entities = Vec::<EntitySchema>::new();
+    let mut indexes = Vec::<IndexSchema>::new();
+
+    for mig in migrations {
+        for action in mig.actions {
+            match action {
+                SchemaAction::AttributeCreate(create) => {
+                    let attr = attributes.iter().find(|c| c.ident == create.schema.ident);
+
+                    if let Some(old) = attr {
+                        if old != &create.schema {
+                            bail!("Duplicate AttributeCreate action for attr {} - merging multiple creates is not supported yet", create.schema.ident);
+                        }
+                    } else {
+                        attributes.push(create.schema);
+                    }
+                }
+                SchemaAction::AttributeUpsert(upsert) => {
+                    let attr = attributes.iter().find(|c| c.ident == upsert.schema.ident);
+
+                    if let Some(old) = attr {
+                        if old != &upsert.schema {
+                            bail!(
+                                "Unsupported AttributeUpsert action for attr {} - merging upsert with previous create is not supported yet", 
+                                  upsert.schema.ident
+                            );
+                        }
+                    } else {
+                        attributes.push(upsert.schema);
+                    }
+                }
+                SchemaAction::AttributeChangeType(change) => {
+                    let attr = attributes
+                        .iter_mut()
+                        .find(|c| c.ident == change.attribute)
+                        .ok_or_else(|| anyhow!("Invalid AttributeChangeType action for attr {}: attribute not created yet", change.attribute))?;
+
+                    attr.value_type = change.new_type;
+                }
+                SchemaAction::AttributeCreateIndex(cindex) => {
+                    let attr = attributes
+                        .iter_mut().find(|a| a.ident == cindex.attribute)
+                        .ok_or_else(|| anyhow!("Invalid AttributeChangeType action for attr {}: attribute not created yet", cindex.attribute))?;
+
+                    if cindex.unique {
+                        attr.unique = true;
+                    } else {
+                        attr.index = true;
+                    }
+                }
+                SchemaAction::AttributeDelete(del) => {
+                    attributes.retain(|a| a.ident != del.name);
+                }
+                SchemaAction::EntityCreate(create) => {
+                    let entity = entities.iter().find(|e| e.ident == create.schema.ident);
+
+                    if let Some(old) = entity {
+                        if old != &create.schema {
+                            bail!("Duplicate EntityCreate action for attr {} - merging multiple creates is not supported yet", create.schema.ident);
+                        }
+                    } else {
+                        entities.push(create.schema);
+                    }
+                }
+                SchemaAction::EntityAttributeAdd(add) => {
+                    let entity = entities.iter_mut().find(|e| e.ident == add.entity)
+                    .ok_or_else(|| anyhow!("Invalid EntityAttributeAdd action for attr {}: entity not created yet", add.attribute))?;
+
+                    let old_attr = entity
+                        .attributes
+                        .iter_mut()
+                        .find(|a| a.attribute == add.attribute.clone().into());
+
+                    if let Some(old) = old_attr {
+                        old.cardinality = add.cardinality;
+                    } else {
+                        entity.attributes.push(schema::EntityAttribute {
+                            attribute: add.attribute.clone().into(),
+                            cardinality: add.cardinality,
+                        });
+                    }
+                }
+                SchemaAction::EntityAttributeChangeCardinality(change) => {
+                    let entity = entities.iter_mut().find(|e| e.ident == change.entity_type)
+                    .ok_or_else(|| anyhow!("Invalid EntityAttributeAdd action for attr {}: entity not created yet", change.attribute))?;
+
+                    let old_attr = entity
+                        .attributes
+                        .iter_mut()
+                        .find(|a| a.attribute == change.attribute.clone().into())
+                        .ok_or_else(|| anyhow!("Invalid EntityAttributeChangeCardinality action for attr {}: attribute not added yet", change.attribute))?;
+
+                    old_attr.cardinality = change.new_cardinality;
+                }
+                SchemaAction::EntityAttributeRemove(remove) => {
+                    let entity = entities.iter_mut().find(|e| e.ident == remove.entity_type)
+                    .ok_or_else(|| anyhow!("Invalid EntityAttributeRemove action for attr {}: entity not created yet", remove.attribute))?;
+
+                    entity
+                        .attributes
+                        .retain(|a| a.attribute != remove.attribute.clone().into());
+                }
+                SchemaAction::EntityUpsert(upsert) => {
+                    let entity = entities.iter().find(|e| e.ident == upsert.schema.ident);
+
+                    if let Some(old) = entity {
+                        if old != &upsert.schema {
+                            bail!(
+                                "Unsupported EntityUpsert action for entity {} - merging upsert with previous create is not supported yet", 
+                                  upsert.schema.ident
+                            );
+                        }
+                    } else {
+                        entities.push(upsert.schema);
+                    }
+                }
+                SchemaAction::EntityDelete(del) => {
+                    entities.retain(|e| e.ident != del.name);
+                }
+                SchemaAction::IndexCreate(create) => {
+                    let old_create = indexes.iter().find(|i| i.ident == create.schema.ident);
+
+                    if let Some(old) = old_create {
+                        if old != &create.schema {
+                            bail!("Duplicate IndexCreate action for attr {} - merging multiple creates is not supported yet", create.schema.ident);
+                        }
+                    } else {
+                        indexes.push(create.schema);
+                    }
+                }
+                SchemaAction::IndexDelete(del) => {
+                    indexes.retain(|i| i.ident != del.name);
+                }
+            }
+        }
+    }
+
+    let attr_create = attributes
+        .into_iter()
+        .map(|a| SchemaAction::from(AttributeCreate { schema: a }));
+    let entity_creates = entities
+        .into_iter()
+        .map(|e| SchemaAction::from(EntityCreate { schema: e }));
+    let index_creates = indexes
+        .into_iter()
+        .map(|i| SchemaAction::from(IndexCreate { schema: i }));
+
+    let main = Migration {
+        name: None,
+        actions: attr_create
+            .chain(entity_creates)
+            .chain(index_creates)
+            .collect(),
+    };
+
+    Ok(main)
 }
