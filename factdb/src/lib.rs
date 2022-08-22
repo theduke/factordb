5b05914fbc14f12d8395e880c1c6adeaa5b59e5f
Christoph Herzog <chris@theduke.at>
2022-08-22 02:28:38 +0200

    Re-export whole submodules in factdb crate

--- factdb/src/lib.rs
+++ factdb/src/lib.rs
@@ -1,5 +1,6 @@
 pub use factor_core::{
     data::{
+        self,
         patch::{Patch, PatchOp},
         value::Value,
         value_type::{ObjectField, ObjectType, ValueType, ValueTypeDescriptor},
@@ -8,12 +9,14 @@ pub use factor_core::{
     db::{Db, DbClient},
     map,
     query::{
+        self,
         expr::Expr,
         migrate::Migration,
         mutate::{Batch, Mutate},
         select::{Item, Order, Page, Select, Sort},
     },
     schema::{
+        self,
         builtin::{AttrId, AttrIdent, AttrType},
         AttrMapExt, Attribute, AttributeMeta, Cardinality, Class, ClassAttribute, ClassContainer,
         ClassMeta, DbSchema,
